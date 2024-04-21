3a0a1dc15e247a24251298c0d899a243e6b306b5
V4l3ntino <valentinohontar15@gmail.com>
2024-04-21 10:44:28 +0200

    signupValidation

--- auth-react/auth-back/app.js
+++ auth-react/auth-back/app.js
@@ -12,6 +12,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+async function main(){
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log("Connected to MongoDB");
+}
+
+main().catch(console.error);
+
 app.use('/api/signup', require('./routes/signup'))
 app.use('/api/login', require('./routes/login'))
 app.use('/api/refresh-token', require('./routes/refreshToken'))
--- auth-react/auth-back/routes/signup.js
+++ auth-react/auth-back/routes/signup.js
@@ -1,24 +1,62 @@
 const router = require("express").Router();
 const { jsonResponse } = require("../lib/jsonResponse");
+const User = require("../schema/user");
 
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const {username, name, password} = req.body;
 
     if(!!!username || !!!name || !!!password){
-        return res.status(400).json(jsonResponse(400, {
+        return res.status(400).json(
+            jsonResponse(400, {
             error:"Fields are required",
-        }))
+          })
+        );
     }
 
+    try {
 
-    //crear usuario en la base de datos
+        const user = new User();
+        const exist = await user.usernameExist(username);
 
-    res
-       .status(200)
-       .json(jsonResponse(200, {message: "User created succesfully"}))
+        if(exist){
+            return res.status(400).json(
+                jsonResponse(400, {
+                    error: "Username already exists"
+                })
+            )
+        }
 
-    res.send("signout");
-})
+        //crear usuario en la base de datos
+
+        async function guardarUsuario() {
+            const user = new User({
+                username,
+                name,
+                password
+            });
+        
+            await user.save();
+        }
+        guardarUsuario();
+        
+
+
+        res
+        .status(200)
+        .json(jsonResponse(200, {message: "User created succesfully"}))
+
+        res.send("signout");
+        
+    } catch (error) {
+        res.status(500).json(
+            jsonResponse(500, {
+                error: "Error creating user"
+            })
+        )
+    }
+    
+    
+});
 
 module.exports = router;
